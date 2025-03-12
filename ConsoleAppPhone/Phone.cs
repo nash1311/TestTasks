831044fb00705d28103a88e3adb8dd93d949b02f
Sherstnev Nikita <sherstnev@seaproject.ru>
2025-03-12 15:59:34 +0300

    dev DipaulC#TestCase

---
+++ ConsoleAppPhone/Phone.cs
@@ -0,0 +1,82 @@
+﻿namespace ConsoleAppPhone
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public static class Phone
+    {
+        private static char[][] _keyBoard = new List<char[]>
+           {
+               new []{' '},            //0
+               new []{_empty},         //1
+               new []{'a','b','c'},    //2    
+               new []{'d','e','f'},    //3
+               new []{'g','h','i'},    //4
+               new []{'j','k','l'},    //5
+               new []{'m','n','o'},    //6
+               new []{'p','q','r','s'},//7
+               new []{'t','u','v'},    //8
+               new []{'w','x','y','z' }//9
+           }.ToArray();
+
+        private const char _delete = '*';
+        private const char _send = '#';
+        private const char _delay = ' ';
+        private const char _empty = default(char);
+
+        public static string ConvertToText(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+            var someString = new StringBuilder(input);
+            var result = new StringBuilder();
+            char current;
+            char? previous = null;
+            var together = 0;
+
+            for (int i = 0; i < someString.Length; i++)
+            {
+                current = someString[i];
+
+                switch (current)
+                {
+                    case _send:
+                        goto Result;
+                    case _delay:
+                        together = 0;
+                        previous = null;
+                        continue;
+                    case _delete:
+                        together = 0;
+                        previous = null;
+                        result.Remove(result.Length - 1, 1);
+                        continue;
+                }
+                if (previous.HasValue && previous == current)
+                {
+                    together++;
+                    result.Remove(result.Length - 1, 1);
+                }
+                else
+                {
+                    together = 0;
+                }
+
+                var valueRange = _keyBoard[Convert.ToInt32(current.ToString())];
+                var value = valueRange[together % valueRange.Length];
+                if (value == _empty)
+                {
+                    previous = null;
+                    continue;
+                }
+                previous = current;
+                result.Append(value);
+            }
+            Result:
+            return result.ToString();
+        }
+    }
+}
---
+++ ConsoleAppPhone/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleAppPhone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = "4444433555555 555666096667775553#";
+            var result = Phone.ConvertToText(inputString);
+            var expectedResult = "hello world";
+
+
+            inputString = "34447288555#";
+            result = Phone.ConvertToText(inputString);
+            expectedResult = "dipaul";
+
+            inputString = "446669086660777733802224427772 2228337770277770336789990444660222#";
+            result = Phone.ConvertToText(inputString);
+            expectedResult = "how to set character as empty in c";
+
+            inputString = "7 7776668884443 3377770207666 66655506663330844777332377770844280222 26602233088777733 308666033993322288 80827777557777#";
+            result = Phone.ConvertToText(inputString);
+            expectedResult = "provides a pool of threads that can be used to execute tasks";
+
+            // нажатие 1 и цикличный нажатие 7
+            inputString = "7 7776668881114443 33333777777770207666 66655506663330844777332377770844280222 26602233088777733 308666033993322288 80827777557777#";
+            result = Phone.ConvertToText(inputString);
+            expectedResult = "provides a pool of threads that can be used to execute tasks";
+
+            //*
+            inputString = "7 7776668884443 33*3377770207666 66655506663330844777332377770844280222 26602233088777733 308666033993322288 80827777557777#";
+            result = Phone.ConvertToText(inputString);
+            expectedResult = "provides a pool of threads that can be used to execute tasks";
+        }
+
+      
+    }
+}
